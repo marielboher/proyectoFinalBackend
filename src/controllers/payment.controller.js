cba5b7f0a3303f1b0342f67307a53b416947e983
marielboher <mariellboheer@gmail.com>
2023-12-18 19:38:11 -0300

    cambio de urls por ralwey

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -50,8 +50,9 @@ export const createPayment = async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `http://localhost:8000/payment/payment-success?session_id={CHECKOUT_SESSION_ID}&cart_id=${cartId}`,
-      cancel_url: "http://localhost:8000/cancel",
+      success_url: `https://proyectofinalbackend-production-630f.up.railway.app/payment/payment-success?session_id={CHECKOUT_SESSION_ID}&cart_id=${cartId}`,
+      cancel_url:
+        "https://proyectofinalbackend-production-630f.up.railway.app/cancel",
     });
 
     res.json(session);
